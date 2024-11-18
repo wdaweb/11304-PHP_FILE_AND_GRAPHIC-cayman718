a4b27f706359dfc6857980dac8ea050f7279b0f9
cayman718 <a44362652@gmail.com>
2024-11-18 10:59:38 +0800

    完成檔案上傳並顯示列表

--- manage.php
+++ manage.php
@@ -6,7 +6,6 @@
  * 4.製作檔案管理功能頁面
  */
 
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,16 +15,58 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>檔案管理功能</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        .item{
+            width: 200px;
+        }
+        .item img{
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
 <h1 class="header">檔案管理練習</h1>
 <!----建立上傳檔案表單及相關的檔案資訊存入資料表機制----->
+<?php
+include_once "function.php";
+// echo $_POST['name'];
+// echo "<br>";
+// dd($_FILES);
+
+if(isset($_FILES['img'])){
+
+
+if($_FILES['img']['error']==0){
+    move_uploaded_file($_FILES['img']['tmp_name'],"./files/".$_FILES['img']['name']);
+}else{
+    echo "上船失敗，請檢查檔案格式或是大小是否符合規定";
+}
+}
+
+?>
 
 
 
 
 
 <!----透過資料表來顯示檔案的資訊，並可對檔案執行更新或刪除的工作----->
+<?php
+
+$dirpath="./files";
+
+//$dir=opendir($dirpath);
+$items=scandir($dirpath);
+$items=array_diff($items,array('.','..'));
+dd($items);
+
+foreach($items as $file){
+    echo "<div class='item'>";
+    echo "<img src='{$dirpath}/{$file}'>";
+    echo "</div>";
+}
+
+?>
+
 
 
 
